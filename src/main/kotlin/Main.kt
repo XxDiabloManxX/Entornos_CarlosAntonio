64e2e74b41d4c13a7eeb522dcc198653f1361fbd
Carlos <carlos.canal.da.silva@gmail.com>
2024-03-15 21:38:20 +0100

    Refac_Final

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,56 +4,36 @@ import java.util.*
 import kotlin.random.Random
 
 fun main() {
-    var m = Array(4) { Array<Int>(4) { Random.nextInt(1, 9)} }
-    var sumaFilas = Array(m.size){0}
-    var sumaColum = Array(m[0].size){0}
+    var matriz = Array(4) { Array<Int>(4) { Random.nextInt(1, 9)} }
+    var sumaFilas = Array(matriz.size){0}
+    var sumaColumas = Array(matriz[0].size){0}
 
 
-    for (f in m.indices){
-        for (c in m[f].indices){
-            print("${m[f][c]} ")
+    for (fila in matriz.indices){
+        for (columna in matriz[fila].indices){
+            print("${matriz[fila][columna]} ")
         }
         println()
     }
-    sumarFilas(m, sumaFilas)
+    sumarArray(matriz, sumaFilas)
+    sumarArray(matriz,sumaColumas)
 
-    sumarColumnas(m,sumaColum)
     println("La suma de las filas es")
     println(Arrays.toString(sumaFilas))
     println("La suma de las columnas es")
-    println(Arrays.toString(sumaColum))
-
-
+    println(Arrays.toString(sumaColumas))
 }
 
+fun sumarArray (m:Array<Array<Int>>, sumaArray:Array<Int>){
+    var fila: Int = 0
+    var columna: Int = 0
 
-
-
-
-fun sumarColumnas (m:Array<Array<Int>>, sumaColumnas:Array<Int>){
-    var f: Int = 0
-    var c: Int = 0
-
-    while (c < m.size){
-        while (f < m[0].size){
-            sumaColumnas[c] += m[f][c]
-            f++
+    while (columna < m.size){
+        while (fila < m[0].size){
+            sumaArray[columna] += m[fila][columna]
+            fila++
         }
-        f = 0
-        c++
-    }
-}
-
-private fun sumarFilas(m: Array<Array<Int>>, sumaFilas: Array<Int>) {
-    var f: Int = 0
-    var c: Int = 0
-
-    while (f < m.size) {
-        while (c < m[0].size) {
-            sumaFilas[f] += m[f][c]
-            c++
-        }
-        c = 0
-        f++
+        fila = 0
+        columna++
     }
 }
